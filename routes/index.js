5f44f3eb406fc1a8d5a16c8fef1c0c481271642c
MJMoquin <mjmoquin@gmail.com>
2020-06-16 09:50:52 -0500

    add ocean page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/ocean', function(req, res) {
+  res.render('ocean');
+})
 
 module.exports = router;
