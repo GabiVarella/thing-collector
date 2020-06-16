3378b7c8d45e58ae46d207a4c1bc93574ef0ca8a
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-06-16 08:08:18 -0700

    gabriel

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/gabriel', function(req, res){
+  res.render('gabriel')
+})
 module.exports = router;
