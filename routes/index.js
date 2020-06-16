ccf97f7ae2f690009c789eb026a5a422a667eb31
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-06-16 10:00:28 -0500

    pokemon

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/pokemon', function(req, res) {
+  res.render('pokemon')
+});
+
 module.exports = router;
